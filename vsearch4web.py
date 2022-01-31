deeb85d528f55043ea9a71430cb211bcd36e171c
Vell <leve1l@yandex.ru>
2022-01-31 23:26:44 +0300

    flusk end

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,12 +1,11 @@
-from crypt import methods
 from flask import Flask, render_template, request
 from vsearch import search4letters
 
 app = Flask(__name__)
 
 
-@app.route('/search4', methods['POST'])
-def do_search() -> 'html':
+@app.route('/search4', methods = ['POST'])
+def do_search():
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
@@ -19,7 +18,7 @@ def do_search() -> 'html':
 
 @app.route('/')
 @app.route('/entry')
-def entry_page() -> 'html':
+def entry_page():
     return render_template('entry.html',
 the_title='Welcome to search4letters on the web!')
 
