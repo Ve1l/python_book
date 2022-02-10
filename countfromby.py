72547f2d28c3cd9876180e57165c0c1dff253773
[ve1l] <[leve1l@yandex.ru]>
2022-02-10 17:19:47 +0300

    create class, write and open viewlog form db

---
+++ countfromby.py
@@ -0,0 +1,20 @@
+class CountFromBy:
+  def __init__(self, v: int=0, i: int=1) -> None:
+      self.val = v
+      self.incr = i
+      
+  def increase(self) -> None:
+    self.val += self.incr
+    
+  def __repr__(self) -> str:
+      return str(self.val)
+    
+
+test = CountFromBy(2, 3)    
+print(test)
+test.increase()
+print(test)
+test.increase()
+print(test)
+test.increase()
+print(test)
