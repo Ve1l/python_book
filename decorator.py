3fa53c4822680878f145df35eda043ddf62d7f4d
[ve1l] <[leve1l@yandex.ru]>
2022-02-14 17:11:04 +0300

    decorator

--- decorator.py
+++ decorator.py
@@ -4,7 +4,7 @@ def decorator_name (func):
     @wraps(func)
     def wrapper(*args, **kwargs):
       #1. Code to ecevute BEFORE calling the decorated function.
-      #2. Вызов декаорируемый функции и возврат
+      #2. Вызов декорируемой функции и возврат
       # полученных от неё результатов.
       return func(*args, **kwargs)
       #3. Код для выполнения ВМЕСТО вызова декорируемой функции.
